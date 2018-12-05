fd8c24faeaf51de889dff7ed5e748f48ba023c15
Imogen Hardy <imogen.hardy@gmail.com>
2018-12-05 21:26:07 +0000

    Day 1 solution

---
+++ src/day1.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+
+const sanitizeInput = () =>
+  readFileAsync('./input.txt').then(buff =>
+    buff
+      .toString()
+      .split('\n')
+      .map(n => parseInt(n))
+      .filter(n => !isNaN(n))
+  );
+
+const sum = (x, y) => x + y;
+
+const partOne = arr => arr.reduce(sum, 0);
+
+sanitizeInput().then(arr => console.log(partOne(arr)));
+
+const partTwo = (arr, startFreqs = new Set([0]), lastLast = 0) => {
+  const dupes = [];
+  const freqs = arr.reduce(
+    ({ sums, last }, n) => {
+      const next = sum(last, n);
+      if (sums.has(next)) {
+        dupes.push(next);
+      }
+      return {
+        sums: sums.add(next),
+        last: next
+      };
+    },
+    { sums: startFreqs, last: lastLast }
+  );
+  if (dupes.length) {
+    return dupes[0];
+  }
+  const freqsArray = [...freqs.sums];
+  const lastFreq = freqsArray[freqsArray.length - 1];
+  return partTwo(arr, freqs.sums, lastFreq);
+};
+
+sanitizeInput().then(arr => console.log(partTwo(arr)));
